54cc00a5967d3c6a85ca5c7624d56209ec6d0a92
asmtask <asmtask@gmai.com>
2016-05-09 07:38:28 +0000

    first commit

---
+++ git.sh
@@ -0,0 +1,11 @@
+
+echo "# ngx_pagespeed-1.8.31.2-beta" >> README.md
+git init
+git add *
+git commit -m "first commit"
+git remote add origin https://github.com/asmlib/ngx_pagespeed-1.8.31.2-beta.git
+git push -u origin master
+
+
+
+
